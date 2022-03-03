f65d3d8f75fa4eb3caaefb615dda58d82f6880fb
Bekanzar007 <saitovbeknazar6@gmail.com>
2022-03-04 00:51:11 +0600

    Adding Resources Send email part

--- vars/sendemail.groovy
+++ vars/sendemail.groovy
@@ -1,4 +1,5 @@
 def call(Map config = [:]) {
     loadLinuxScript (name: 'sendemail.sh')
-    sh "./sendemail.sh    ${config.name} ${config.surname}."
+    // sh "./sendemail.sh    ${config.name} ${config.surname}."
+    sh 'mail bcc: '', body: 'Please make sure that you started your build', cc: '', from: '', replyTo: '', subject: 'Your build started', to: 'esenbaevnurmuhamed7@gmail.com''
 }
